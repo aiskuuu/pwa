22920de754023c9020bc34a98a78b0a9966ad7cd
aiskuuu <facebookhula2020@gmail.com>
2025-07-31 01:06:05 +0700

    Update config.js
    cfg

--- public/js/config.js
+++ public/js/config.js
@@ -1,5 +1,5 @@
 //config url
 const BASE_API_URL = window.location.hostname.includes("localhost")
   ? "http://localhost:3000"
-:"https://0f72ee4fb51d.ngrok-free.app";
+:"https://pwa-entry.zeabur.app/";
 
