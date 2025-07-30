12e17904b63e82b1cb59b5a8594c7934d63ae5d3
aiskuuu <facebookhula2020@gmail.com>
2025-07-31 01:05:20 +0700

    Update server.js
    port

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 const { google } = require('googleapis');
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.use(cors());
 app.use(bodyParser.json());
