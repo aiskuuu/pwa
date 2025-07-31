b9fe1c3573979162e25cf4e222c6a1e800509f52
aiskuuu <facebookhula2020@gmail.com>
2025-07-31 10:25:10 +0700

    Update db.js
    update mulu anjg

--- backend/db.js
+++ backend/db.js
@@ -3,9 +3,10 @@ const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
   host: 'dirgantara.arenhost.com',
+  port: 2083, // <- WAJIB ditambahkan karena defaultnya 3306
   user: 'rosttoin_rizky',
-  password: 'Rizkyaisyah1997',       // sesuaikan dengan password MySQL kamu
-  database: 'rizky' // ganti dengan nama database kamu
+  password: 'Rizkyaisyah1997', // jaga kerahasiaannya nanti ya
+  database: 'rizky' // pastikan ini nama database kamu
 });
 
 connection.connect(err => {
